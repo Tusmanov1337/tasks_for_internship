f078c62a259f18285ccb3d43c4aa12ca2703d1bb
Tusmanov1337 <tusmanov@bk.ru>
2025-07-23 16:53:45 +0300

    Update task_10.py
    Забыл print удалить

--- task_10.py
+++ task_10.py
@@ -3,7 +3,6 @@ import re
 def count_words(string):
     result = {}
     cleaned_str = (re.sub(r'[^a-zA-Z]', ' ', string)).lower()
-    print(cleaned_str)
     for word in cleaned_str.split():
         result.setdefault(word, 0)
         result[word] += 1
